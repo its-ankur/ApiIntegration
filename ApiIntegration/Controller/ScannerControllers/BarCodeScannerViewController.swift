724c3f01ddb3f7194d7b4947eb435d464739666a
its-ankur <ankuratul2007@gmail.com>
2024-10-08 13:25:07 +0530

    functionally to share the scanned QR or bar code result implemented

--- ApiIntegration/Controller/ScannerControllers/BarCodeScannerViewController.swift
+++ ApiIntegration/Controller/ScannerControllers/BarCodeScannerViewController.swift
@@ -8,7 +8,8 @@ class BarCodeScannerViewController: UIViewController, AVCaptureMetadataOutputObj
     
     var captureSession: AVCaptureSession!
     var previewLayer: AVCaptureVideoPreviewLayer!
-
+    var scannedResult: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,50 +22,50 @@ class BarCodeScannerViewController: UIViewController, AVCaptureMetadataOutputObj
         // Setup camera session
         setupCamera()
     }
-
+    
     private func setupCamera() {
         captureSession = AVCaptureSession()
-
+        
         guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
         let videoInput: AVCaptureDeviceInput
-
+        
         do {
             videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
         } catch {
             return
         }
-
+        
         if (captureSession.canAddInput(videoInput)) {
             captureSession.addInput(videoInput)
         } else {
             return
         }
-
+        
         let metadataOutput = AVCaptureMetadataOutput()
-
+        
         if (captureSession.canAddOutput(metadataOutput)) {
             captureSession.addOutput(metadataOutput)
-
+            
             metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
             metadataOutput.metadataObjectTypes = [.ean13, .ean8, .upce, .qr] // Include QR codes if needed.
         } else {
             return
         }
-
+        
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.frame = barCodeView.layer.bounds
         previewLayer.videoGravity = .resizeAspectFill
         barCodeView.layer.addSublayer(previewLayer) // Corrected to add the layer to barCodeView
-
+        
         // Start capture session on a background thread
         DispatchQueue.global(qos: .userInitiated).async {
             self.captureSession.startRunning()
         }
     }
-
+    
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         captureSession.stopRunning()
-
+        
         if let metadataObject = metadataObjects.first {
             guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
             guard let stringValue = readableObject.stringValue else { return }
@@ -72,25 +73,29 @@ class BarCodeScannerViewController: UIViewController, AVCaptureMetadataOutputObj
             displayResult(result: stringValue)
         }
     }
-
+    
     func displayResult(result: String) {
         // Display the scanned result in the label
         self.result.text = "Scanned Result: " + result
-
+        scannedResult = result  // Store the scanned result
+        
         // Optionally, you can also show an alert
         let alert = UIAlertController(title: "Bar Code Scanned", message: result, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+            // Resume scanning after alert dismissed
+            self.captureSession.startRunning()
+        })
         present(alert, animated: true, completion: nil)
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
+        
         if (captureSession.isRunning) {
             captureSession.stopRunning()
         }
     }
-
+    
     @IBAction func backButtonPressed(_ sender: UIButton) {
         print("Back button tapped") // Debugging statement
         // Instantiate a new MyVisitsController
@@ -102,5 +107,27 @@ class BarCodeScannerViewController: UIViewController, AVCaptureMetadataOutputObj
         // Push the new MyVisitsController onto the navigation stack
         self.navigationController?.pushViewController(myVisitsVC, animated: true)
     }
+    
+    @IBAction func shareButton(_ sender: UIButton) {
+        guard let resultToShare = scannedResult else {
+            // Optionally, you can show an alert that there is no result to share
+            let alert = UIAlertController(title: "No Result", message: "Please scan a Bar code first.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        // Create the share activity
+        let activityViewController = UIActivityViewController(activityItems: [resultToShare], applicationActivities: nil)
+        
+        // Exclude certain activity types if needed
+        activityViewController.excludedActivityTypes = [UIActivity.ActivityType.addToReadingList, UIActivity.ActivityType.postToFlickr]
+        
+        // Present the share sheet
+        present(activityViewController, animated: true, completion: nil)
+
+    }
+    
+    
 }
 
--- ApiIntegration/Controller/ScannerControllers/QRScannerViewController.swift
+++ ApiIntegration/Controller/ScannerControllers/QRScannerViewController.swift
@@ -2,13 +2,15 @@ import AVFoundation
 import UIKit
 
 class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
-
+    
     @IBOutlet weak var qrCodeView: UIView!  // Camera preview area
     @IBOutlet weak var Result: UILabel!      // Label to display the scanned QR code
     
     var captureSession: AVCaptureSession!
     var previewLayer: AVCaptureVideoPreviewLayer!
+    var scannedResult: String?
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,60 +19,60 @@ class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsD
         
         // Hide the back button
         self.navigationItem.hidesBackButton = true
-
+        
         
         // Setup camera session
         setupCamera()
         
     }
-
+    
     private func setupCamera() {
         captureSession = AVCaptureSession()
-
+        
         guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
         let videoInput: AVCaptureDeviceInput
-
+        
         do {
             videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
         } catch {
             return
         }
-
+        
         if (captureSession.canAddInput(videoInput)) {
             captureSession.addInput(videoInput)
         } else {
             return
         }
-
+        
         let metadataOutput = AVCaptureMetadataOutput()
-
+        
         if (captureSession.canAddOutput(metadataOutput)) {
             captureSession.addOutput(metadataOutput)
-
+            
             metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
             metadataOutput.metadataObjectTypes = [.qr]
         } else {
             return
         }
-
+        
         // Update previewLayer to fit qrCodeView
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.frame = qrCodeView.layer.bounds // Use qrCodeView's bounds
         previewLayer.videoGravity = .resizeAspectFill
         qrCodeView.layer.addSublayer(previewLayer) // Add preview layer to qrCodeView
-
+        
         // Start capture session on a background thread
         DispatchQueue.global(qos: .userInitiated).async {
             self.captureSession.startRunning()
         }
     }
-
+    
     
     // MARK: - QR Code Scanning
-
+    
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         captureSession.stopRunning()
-
+        
         if let metadataObject = metadataObjects.first {
             guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
             guard let stringValue = readableObject.stringValue else { return }
@@ -78,10 +80,11 @@ class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsD
             displayResult(result: stringValue)
         }
     }
-
+    
     func displayResult(result: String) {
         // Update the Result label with the scanned QR code value
         Result.text = "Scanned Result: " + result
+        scannedResult = result  // Store the scanned result
         
         // Optionally, show an alert if needed
         let alert = UIAlertController(title: "QR Code Scanned", message: result, preferredStyle: .alert)
@@ -91,10 +94,10 @@ class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsD
         })
         present(alert, animated: true, completion: nil)
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
+        
         if (captureSession.isRunning) {
             captureSession.stopRunning()
         }
@@ -112,5 +115,27 @@ class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsD
         // Push the new MyVisitsController onto the navigation stack
         self.navigationController?.pushViewController(myVisitsVC, animated: true)
     }
+    
+    
+    @IBAction func shareButton(_ sender: UIButton) {
+        guard let resultToShare = scannedResult else {
+            // Optionally, you can show an alert that there is no result to share
+            let alert = UIAlertController(title: "No Result", message: "Please scan a QR code first.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        // Create the share activity
+        let activityViewController = UIActivityViewController(activityItems: [resultToShare], applicationActivities: nil)
+        
+        // Exclude certain activity types if needed
+        activityViewController.excludedActivityTypes = [UIActivity.ActivityType.addToReadingList, UIActivity.ActivityType.postToFlickr]
+        
+        // Present the share sheet
+        present(activityViewController, animated: true, completion: nil)
+    }
+
+    
 }
 
